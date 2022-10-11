fb73c117ebba11a76f8829c29ba4e58bdc6a03dc
Ricardo <r.goncalo@campus.fct.unl.pt>
2022-10-11 15:23:32 +0100

    Reduced particle size and incresed its number

--- projects/project-1/app.js
+++ projects/project-1/app.js
@@ -7,7 +7,7 @@ let inParticlesBuffer, outParticlesBuffer, quadBuffer;
 // Particle system constants
 
 // Total number of particles
-const N_PARTICLES = 10000;
+const N_PARTICLES = 50000;
 
 let drawPoints = true;
 let drawField = true;
