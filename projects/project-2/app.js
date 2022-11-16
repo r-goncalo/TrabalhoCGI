3e6ea859369a68823612ddc1b12e2cdedea1e113
Ricardo <r.goncalo@campus.fct.unl.pt>
2022-11-16 23:27:45 +0000

    Added two helicopters to showcase the code

--- projects/project-2/app.js
+++ projects/project-2/app.js
@@ -11,15 +11,18 @@ let gl;
 const VP_DISTANCE = 500; //500 meters far
 let time = 0;           // Global simulation time
 let speed = 1/60.0;     // Speed (how many days added to time on each render pass
-let mode;
 
 
 //the cameras we'll have, composed of function that return a view matrix
 let cameras = [];
-let currentCamera;
+let currentCamera = 0;
+
+//draw modes
+let drawModes = [];
+let currentDrawMode = 0;
 
 
-//instanceTree will be composed by {model : function,  coord: [x, y, z], rotation : [x, y, z], scale : [x, y, z] filhos : [] }
+//instanceTree will be composed by {name : string, model : function,  coord: [x, y, z], rotation : [x, y, z], scale : [x, y, z] filhos : [] }
 let instanceTree = [];
 
 //used to find instances by name
@@ -29,31 +32,75 @@ let instanceDic = {};
 //note: the instances will be able to use "this" becase they're called from the body of the instance
 let activeInstances = [];
 
+function generateInstanceName(newName){
+
+    let toReturn = newName;
+    for(let i = 2; instanceDic[toReturn] != undefined; i++){
+
+        toReturn = newName + "_" + i;
+
+    }
+
+    return toReturn;
+
+
+}
+
 function addInstance(nameStr, modelFun, initialCoord){
 
-    let instance = { model : modelFun, coord : initialCoord, rotation : [0, 0, 0], scale : [1, 1, 1], filhos : []};
-    instanceDic[nameStr] = instance;
+    let newName = generateInstanceName(nameStr);
+
+    let instance = { name : newName, model : modelFun, coord : initialCoord, rotation : [0, 0, 0], scale : [1, 1, 1], filhos : []};
+    instanceDic[newName] = instance;
     instanceTree.push(instance);
+
+
+    return instance;
 
 }
 
 function addInstanceSon(nameStr, modelFun, initialCoord, parentName){
 
     let instanceParent = instanceDic[parentName];
-    let instance = { model : modelFun, coord : initialCoord, rotation : [0, 0, 0], scale : [1, 1, 1], filhos : []};
-    instanceDic[nameStr] = instance;
+    
+    let newName = generateInstanceName(nameStr);
+
+    let instance = { name : newName, model : modelFun, coord : initialCoord, rotation : [0, 0, 0], scale : [1, 1, 1], filhos : []};
+
+    instanceDic[newName] = instance;
     instanceParent.filhos.push(instance);
+
+
+    return instance;
 
 }
 
+function addActiveInstance(nameStr, modelFun, animateFun, initialCoord){
+
+    let newName = generateInstanceName(nameStr);
+
+    let instance = { name : newName, model : modelFun, animate : animateFun, coord : initialCoord, rotation : [0, 0, 0], scale : [1, 1, 1], filhos : []};
+    instanceDic[newName] = instance;
+    activeInstances.push(instance);
+    instanceTree.push(instance);
+
+    return instance;
+
+
+}
 
 function addActiveInstanceSon(nameStr, modelFun, animateFun, initialCoord, parentName){
 
     let instanceParent = instanceDic[parentName];
-    let instance = { model : modelFun, coord : initialCoord, animate : animateFun, rotation : [0, 0, 0], scale : [1, 1, 1], filhos : []};
-    instanceDic[nameStr] = instance;
+
+    let newName = generateInstanceName(nameStr);
+
+    let instance = { name : newName, model : modelFun, animate : animateFun,  coord : initialCoord, rotation : [0, 0, 0], scale : [1, 1, 1], filhos : []};
+    instanceDic[newName] = instance;
     instanceParent.filhos.push(instance);
     activeInstances.push(instance);
+
+    return instance;
 
 }
 
@@ -114,6 +161,7 @@ function setup(shaders)
                 break;
             //tecla S - superficies preenchidas
             case 'S':
+                currentDrawMode = (currentDrawMode + 1) % drawModes.length;
                 break;
             //A tecla 1 devera voltar a usar a projecao axonometrica
             case '1':
@@ -156,7 +204,6 @@ function setup(shaders)
 
     */
 
-    //DO EXERCICIO 18
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     SPHERE.init(gl);
@@ -176,8 +223,8 @@ function setup(shaders)
     }
 
 
-        
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    //this will define the color of the sky
+    gl.clearColor(0.1, 0.3, 0.8, 1.0);
     CUBE.init(gl);
     gl.enable(gl.DEPTH_TEST);   // Enables Z-buffer depth test
     
@@ -199,6 +246,19 @@ function setup(shaders)
         gl.uniformMatrix4fv(gl.getUniformLocation(program, "mModelView"), false, flatten(modelView()));
     }
 
+
+    /*
+        *****SETUP DRAW MODES***
+    */
+
+    drawModes = [gl.LINES, gl.TRIANGLES];
+    currentDrawMode = 0;
+
+    /*
+        *****END OF SETUP DRAW MODES***
+    */    
+
+
     /*
         *****SETUP INITIAL INSTANCES***
     */
@@ -213,7 +273,7 @@ function setup(shaders)
             multScale([1000, 5, 5]);
             uploadModelView();
             defineColor(1, 0, 0); //red
-            CUBE.draw(gl, program, mode);
+            CUBE.draw(gl, program, drawModes[currentDrawMode]);
     
         }
     
@@ -223,7 +283,7 @@ function setup(shaders)
             multScale([5, 1000, 5]);
             uploadModelView();
             defineColor(0, 0, 1);//blue
-            CUBE.draw(gl, program, mode);
+            CUBE.draw(gl, program, drawModes[currentDrawMode]);
     
         }
     
@@ -232,7 +292,7 @@ function setup(shaders)
             multScale([5, 5, 1000]);
             uploadModelView();
             defineColor(0, 1, 0);//green
-            CUBE.draw(gl, program, mode);
+            CUBE.draw(gl, program, drawModes[currentDrawMode]);
         }
         
         //a refer to know the coordinates
@@ -250,25 +310,57 @@ function setup(shaders)
             
         }
 
+    
+        function setupReferencial(){
+
+            addInstance("Referencial",
+            referencial,
+            [0, 0, 0]);
+
+        }
+
     /*
         ***** END OF SETUP REFERENCIAL***
     */
 
 
+    /*
+        *****SETUP GROUND***
+    */
+
+
+    /*
+        *****SETUP BUILDINGS***
+    */
+
+        
+
+    /*
+        *****END OF SETUP BUILDINGS***
+    */
+
+
+    /*
+        *****END OF SETUP GROUND***
+    */
+
 
     /*
         *****SETUP HELICOPTER***
+
+        An Helicopter (the main body) also as:
+            distance : int
+            speed : int
+
     */
 
-    let helicopterSpeed = 0.5;
-    let helicopterDistance = 300;
     
         function modelMainBody(){
 
             multScale([5, 3, 3]); //the main body is 20 meters, its a giant helicopter
             uploadModelView();
             defineColor(1, 0, 0); //red
-            SPHERE.draw(gl, program, mode);
+            SPHERE.draw(gl, program, drawModes[currentDrawMode]);
 
         }
 
@@ -277,7 +369,7 @@ function setup(shaders)
             multScale([6, 1, 1]); 
             uploadModelView();
             defineColor(1, 0, 0); //red
-            SPHERE.draw(gl, program, mode);
+            SPHERE.draw(gl, program, drawModes[currentDrawMode]);
 
         }
 
@@ -286,7 +378,7 @@ function setup(shaders)
             multScale([2, 0.5, 0.5]); 
             uploadModelView();
             defineColor(1, 1, 1); //red
-            SPHERE.draw(gl, program, mode);
+            SPHERE.draw(gl, program, drawModes[currentDrawMode]);
 
         }
 
@@ -295,7 +387,7 @@ function setup(shaders)
             multScale([0.2, 2, 0.2]); 
             uploadModelView();
             defineColor(1, 0, 1); 
-            SPHERE.draw(gl, program, mode);
+            SPHERE.draw(gl, program, drawModes[currentDrawMode]);
 
         }
 
@@ -304,7 +396,7 @@ function setup(shaders)
             multScale([4, 0.1, 0.3]); 
             uploadModelView();
             defineColor(0, 0, 1); 
-            SPHERE.draw(gl, program, mode);
+            SPHERE.draw(gl, program, drawModes[currentDrawMode]);
 
         }
     
@@ -317,55 +409,62 @@ function setup(shaders)
 
     }
 
+    let helicopterSpeed = 30;
+    let helicopterDistance = 300;
 
     function animateHelicopter(){
 
+        let angle = (time * this.speed) % 720;
+        let angleInRad = angle * (Math.PI/180);
 
-        this.coord = [helicopterDistance * Math.cos(time * helicopterSpeed),
+        this.coord = [this.distance * Math.cos(angleInRad),
                       this.coord[1],
-                      helicopterDistance * Math.sin(time * helicopterSpeed)];
+                      this.distance * Math.sin(angleInRad)];
 
-        this.rotation[0] = time * helicopterSpeed; //why isn't this working?
-
+        this.rotation[1] = 180 - angle; //why isn't this working?
 
 
     }
 
-    function setupHelicopter(){
+    function setupHelicopter(helicopterDistance, helicopterSpeed){
 
-        addActiveInstanceSon("Helicopter",
+        let helicoinstance = addActiveInstance("Helicopter",
         modelMainBody,
         animateHelicopter,
-        [0, 400, 0], //only the Y matters because of the animation
-        "World");
+        [0, 400, 0]);
 
-            addInstanceSon("HelicopterTail",
+        helicoinstance.distance = helicopterDistance;
+        helicoinstance.speed = helicopterSpeed;
+
+            let hTail = addInstanceSon("HelicopterTail",
             modelTail,
             [-4, 0, 0],
-            "Helicopter");
+            helicoinstance.name);
 
-                addInstanceSon("HelicopterTailPoint",
+                let hTailPoint = addInstanceSon("HelicopterTailPoint",
                 modelTailPoint,
                 [-3, 0.5, 0],
-                "HelicopterTail");
+                hTail.name);
 
-                instanceDic["HelicopterTailPoint"].rotation[2] = -75;
+                hTailPoint.rotation[2] = -75;
 
-            addActiveInstanceSon("HelicopterSpike",
+            let hSpike = addActiveInstanceSon("HelicopterSpike",
             modelSpike,
             animateHelicopterHeliceRotation,
             [0, 2, 0],
-            "Helicopter");
+            helicoinstance.name);
 
                 addInstanceSon("HelicopterHelice1",
                 helicopterHelice,
                 [2, 0.5, 0],
-                "HelicopterSpike");
+                hSpike.name);
 
                 addInstanceSon("HelicopterHelice2",
                 helicopterHelice,
                 [-2, 0.5, 0],
-                "HelicopterSpike");
+                hSpike.name);
+
+        scaleInstanceByValue(helicoinstance.name, 5);
 
     }
     
@@ -377,31 +476,26 @@ function setup(shaders)
         
     function setupInstances(){
 
-
-        addInstance("World",
-                    function(){},
-                    [0, 0, 0]);
     
-        addInstanceSon("Ground",
+
+        setupReferencial();
+
+        addInstance("Ground",
                     function(){
                         multTranslation([0, -5, 0]);
                         multScale([1000, 5, 1000]);
                         uploadModelView();
                         defineColor(1, 1, 1); //white
-                        CUBE.draw(gl, program, mode);
+                        CUBE.draw(gl, program, drawModes[currentDrawMode]);
                     },
-                    [0, 0, 0],
-                    "World");
+                    [0, 0, 0]);
 
-        addInstanceSon("Referencial",
-        referencial,
-        [0, 0, 0],
-        "World");
 
-        setupHelicopter();
 
-        //the helicopter was too small, increasing its size to 3x
-        scaleInstanceByValue("Helicopter", 30);
+        setupHelicopter(300, 30);
+        
+        setupHelicopter(200, 5);
+
 
     
         console.log(instanceTree);
@@ -430,6 +524,7 @@ function setup(shaders)
         *****END OF SETUP CAMERAS***
     */    
 
+        
 
     function recursiveModelConstruction(instanceNodes){
 
